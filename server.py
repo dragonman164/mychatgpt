d9c61155aaf0151fe8958b6d83b413b87f0a6b2f
dragonman164 <sanidhiyafirefox123@gmail.com>
2025-06-11 10:05:14 +0530

    code changes

--- server.py
+++ server.py
@@ -1,29 +1,24 @@
-from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain_core.prompts import ChatPromptTemplate
-from langchain_core.output_parsers import StrOutputParser
+# from langchain_google_genai import ChatGoogleGenerativeAI
+# from langchain_core.prompts import ChatPromptTemplate
+# from langchain_core.output_parsers import StrOutputParser
+
+import google.generativeai as genai
 
 import streamlit as st
 import os
 from dotenv import load_dotenv
 
+
 # ENV Setup
 load_dotenv()
 os.environ["GEMINI_API_KEY"] = os.getenv("GEMINI_API_KEY")
-os.environ["LANGCHAIN_API_KEY"] = os.getenv("LANGCHAIN_API_KEY")
-# Enabling LangSmith Tracing
-os.environ["LANGCHAIN_TRACING_V2"] = "true"
+# os.environ["LANGCHAIN_API_KEY"] = os.getenv("LANGCHAIN_API_KEY")
+# # Enabling LangSmith Tracing
+# os.environ["LANGCHAIN_TRACING_V2"] = "true"
 
 # Prompt Template
 question = "What is the capital of France?"
-prompt = ChatPromptTemplate.from_messages([
-    ("system", "You are a helpful chat assistant. Please answer the user queries.?"),
-    ("user", "Question: {question}"),
-])
-
-# Gemini Call 
-llm = ChatGoogleGenerativeAI(model="gemini-2.0-flash", model_kwargs={"generation_config": {"temperature": 0.1, "top_p": 1, "top_k": 1, "max_output_tokens": 1024}})
-output_parser = StrOutputParser()
-chain = prompt|llm|output_parser
+model = genai.GenerativeModel('gemini-2.5-flash-preview-04-17')
 
 # Streamlit UI
 st.title("Gemini Chat Assistant")
@@ -31,6 +26,6 @@ st.title("Gemini Chat Assistant")
 user_question = st.text_input("Ask a question:", value=question)
 
 if st.button("Get Answer"):
-    response = chain.invoke({"question": user_question})
+    response = model.generate_content(user_question).text
     st.write("**Answer:**", response)
 
